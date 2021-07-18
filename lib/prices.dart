c218ae53f637466e85819eb5983f86b1d6f1bcfd
Lincoln Arrais <lincoln_arrais@hotmail.com>
2021-07-18 10:32:09 -0300

    add constantes dos preços

---
+++ lib/prices.dart
@@ -0,0 +1,18 @@
+class Prices {
+  // Pizzas
+  static const pizzaComum = 15.00;
+  static const pizzaPremium = 20.00;
+
+  // Pastéis
+  static const pastelP = 7.00;
+  static const pastelG = 9.00;
+  static const pastelForno = 12.00;
+
+  // Refrigerantes
+  static const delrioGuarana1l = 5.00;
+  static const antarcticaGuarana1l = 6.00;
+  static const coca600ml = 5.00;
+
+  // Bordas
+  static const bordaPizza = 3.00;
+}
